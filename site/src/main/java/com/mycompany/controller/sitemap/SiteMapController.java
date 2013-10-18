604a9880666762a18100ba50a577802202b3b8ac
Joshua Skorton <jskorton@broadleafcommerce.com>
2013-10-17 19:50:38 -0500

    Updated site map generator controller

--- site/src/main/java/com/mycompany/controller/sitemap/SiteMapController.java
+++ site/src/main/java/com/mycompany/controller/sitemap/SiteMapController.java
@@ -2,9 +2,12 @@ package com.mycompany.controller.sitemap;
 
 import org.broadleafcommerce.common.sitemap.controller.BroadleafSiteMapController;
 import org.broadleafcommerce.common.sitemap.exception.SiteMapException;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
@@ -18,7 +21,6 @@ import javax.servlet.http.HttpServletResponse;
  * @author Joshua Skorton (jskorton)
  */
 @Controller
-@RequestMapping("/sitemap")
 public class SiteMapController extends BroadleafSiteMapController {
 
     /**
@@ -32,21 +34,38 @@ public class SiteMapController extends BroadleafSiteMapController {
      * @throws IOException
      */
     @RequestMapping("/generate")
+    @ResponseBody
     public String generateSiteMap(HttpServletRequest request, HttpServletResponse response, Model model) throws SiteMapException, IOException {
         return super.generateSiteMap(request, response, model);
     }
 
     /**
-     * Retrieves site map
+     * Retrieves a site map index file in XML format
      * 
      * @param request
      * @param response
      * @param model
+     * @param fileName
      * @return
      */
-    @RequestMapping("/retrieve")
-    public String retrieveSiteMap(HttpServletRequest request, HttpServletResponse response, Model model) {
-        return super.retrieveSiteMap(request, response, model);
+    @RequestMapping("/retrieve/{file_name:.+\\.xml}")
+    @ResponseBody
+    public FileSystemResource retrieveSiteMapIndex(HttpServletRequest request, HttpServletResponse response, Model model, @PathVariable(value = "file_name") String fileName) {
+        return super.retrieveSiteMapIndex(request, response, model, fileName);
+    }
+
+    /**
+     * Retrieves a site map file in gzip format
+     * 
+     * @param request
+     * @param response
+     * @param model
+     * @param fileName
+     * @return
+     */
+    @RequestMapping("/retrieve/{file_name:.+\\.gz}")
+    public void retrieveSiteMap(HttpServletRequest request, HttpServletResponse response, Model model, @PathVariable(value = "file_name") String fileName) {
+        super.retrieveSiteMap(request, response, model, fileName);
     }
 
 }
