abebda13ee73903df5e85d332b7b7b68a914bb05
mariofox <mariofox@gmail.com>
2013-12-09 17:54:27 -0500

    - Workflow de AddItem corregido: Sólo se agrega actividad personalizada - Refactor nombres en activities

--- site/src/main/java/com/suramericana/order/service/workflow/SuraAddItemOrderCheckInventoryActivity.java
+++ site/src/main/java/com/suramericana/order/service/workflow/SuraAddItemOrderCheckInventoryActivity.java
@@ -3,7 +3,7 @@ package com.suramericana.order.service.workflow;
 import org.broadleafcommerce.core.order.service.workflow.CartOperationContext;
 import org.broadleafcommerce.core.workflow.BaseActivity;
 
-public class SuraAddItemOrderCheckInventory extends BaseActivity<CartOperationContext> {
+public class SuraAddItemOrderCheckInventoryActivity extends BaseActivity<CartOperationContext> {
 
 	@Override
 	public CartOperationContext execute(CartOperationContext context) throws Exception {
--- site/src/main/java/com/suramericana/order/service/workflow/SuraUpdateItemOrderCheckInventoryActivity.java
+++ site/src/main/java/com/suramericana/order/service/workflow/SuraUpdateItemOrderCheckInventoryActivity.java
@@ -3,7 +3,7 @@ package com.suramericana.order.service.workflow;
 import org.broadleafcommerce.core.order.service.workflow.CartOperationContext;
 import org.broadleafcommerce.core.workflow.BaseActivity;
 
-public class SuraUpdateItemOrderCheckInventory extends BaseActivity<CartOperationContext> {
+public class SuraUpdateItemOrderCheckInventoryActivity extends BaseActivity<CartOperationContext> {
 
 	@Override
 	public CartOperationContext execute(CartOperationContext context) throws Exception {
